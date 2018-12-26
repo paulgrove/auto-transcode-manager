29ef6ca86a3890c7c45695d5c7a5d502c1d3081e
Paul Grove <paul.a.grove@gmail.com>
2018-12-26 16:39:49 +0000

    fix tabs

--- mediainfo-parser.js
+++ mediainfo-parser.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
 exports.exec = exports.parse = undefined;
 exports.default = parse;
@@ -19,52 +19,52 @@ var execa = require("execa");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var isNumeric = function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 var mapValue = function mapValue(value) {
-  var ret = Array.isArray(value) ? value[0] : value;
-  return isNumeric(ret) ? ret * 1 : ret;
+	var ret = Array.isArray(value) ? value[0] : value;
+	return isNumeric(ret) ? ret * 1 : ret;
 };
 
 var transformResponse = function transformResponse(res) {
-  /* eslint-disable no-param-reassign */
-  if (res && res.file) {
-    var tracks = Array.isArray(res.file.track) ? res.file.track : [res.file.track];
-    res.file.track = tracks.map(function (track) {
-      return (0, _lodash.mapValues)(track, mapValue);
-    });
-  }
-    //console.log(">", res);
-  return res;
+	/* eslint-disable no-param-reassign */
+	if (res && res.file) {
+		var tracks = Array.isArray(res.file.track) ? res.file.track : [res.file.track];
+		res.file.track = tracks.map(function (track) {
+			return (0, _lodash.mapValues)(track, mapValue);
+		});
+	}
+		//console.log(">", res);
+	return res;
 };
 
 function parse(buffer) {
-  var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
-  return new Promise((resolve, reject) => {
-      (0, _xml2js.parseString)(buffer, {
-        attrNameProcessors: [function (name) {
-          return '_' + name;
-        }],
-        explicitArray: false,
-        explicitRoot: false,
-        mergeAttrs: true,
-        normalizeTags: true,
-        tagNameProcessors: [_lodash.camelCase]
-      }, function (err, obj) {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(transformResponse(obj));
-      });
-  });
+	var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {};
+	return new Promise((resolve, reject) => {
+			(0, _xml2js.parseString)(buffer, {
+	attrNameProcessors: [function (name) {
+		return '_' + name;
+	}],
+	explicitArray: false,
+	explicitRoot: false,
+	mergeAttrs: true,
+	normalizeTags: true,
+	tagNameProcessors: [_lodash.camelCase]
+			}, function (err, obj) {
+	if (err) {
+		reject(err);
+		return;
+	}
+	resolve(transformResponse(obj));
+			});
+	});
 }
 
 async function exec(mediaPath) {
-  const {stdout} = await execa('mediainfo', ['--Full', '--Output=XML', mediaPath]);
-  //console.log(stdout);
-  return await parse(new Buffer.from(stdout, "utf8"));
+	const {stdout} = await execa('mediainfo', ['--Full', '--Output=XML', mediaPath]);
+	//console.log(stdout);
+	return await parse(new Buffer.from(stdout, "utf8"));
 }
 
 exports.parse = parse;
